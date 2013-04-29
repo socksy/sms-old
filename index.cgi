f3faeba48858e90239188dbe1ca22c7aedd25c8b
Ben Lovell <bjl7@st-andrews.ac.uk>
2013-04-29 13:09:14 +0100

    SMS all works, ZeroMQ stuff added, woo, yay

---
+++ index.cgi
@@ -0,0 +1,8 @@
+#!/cs/home/bjl7/Envs/sms/bin/python
+import site
+site.addsitedir("/cs/home/bjl7/Envs/sms/lib/python2.6/site-packages")
+#print "Content-type: text/html\n\n"
+from wsgiref.handlers import CGIHandler
+from sms import app
+
+CGIHandler().run(app)
--- sms.py
+++ sms.py
@@ -1,22 +1,22 @@
 import zmq
-from flask import Flask, request, redirect
+from flask import Flask, request#, redirect
 import twilio.twiml
-import re
  
 app = Flask(__name__)
 address = "tcp://localhost:56890"
   
 @app.route("/", methods=['GET', 'POST'])
 def hello_monkey():
+    #text = None
     #print request.values
-    text = request.values.get('Body',None)
-    if text:
-        context = zmq.Context()
-        socket = context.socket(zmq.PAIR)
-        socket.connect(address)
-        socket.send(text)
+    context = zmq.Context()
+    socket = context.socket(zmq.PAIR)
+    
+    socket.connect(address)
+    socket.send('from: ' + str(request.form.get('From', '')) + '\nmessage: ' + str(request.form.get('Body', '')), zmq.NOBLOCK)
     resp = twilio.twiml.Response()
     #resp.sms("Hello, Mobile Monkey")
+
     return str(resp)
                 
 if __name__ == "__main__":
